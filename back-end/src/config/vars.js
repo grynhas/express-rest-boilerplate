9239e3b941eb17d022b24e83ae2227faa3ef6008
Patrick Reis <patrickreis@MacBook-Pro-de-Admin.local>
2022-08-23 22:00:53 -0300

    primeiro commit

--- back-end/src/config/vars.js
+++ back-end/src/config/vars.js
@@ -3,7 +3,7 @@ const path = require('path');
 // import .env variables
 require('dotenv-safe').config({
   path: path.join(__dirname, '../../.env'),
-  example: path.join(__dirname, '../../.env.example'),
+  example: path.join(__dirname, '../../.env'),
 });
 
 module.exports = {
